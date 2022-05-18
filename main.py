a58c5c862e552992e11955ca285edd903bd5d097
Matt-2112 <93004048+Matt-2112@users.noreply.github.com>
2022-05-18 16:38:24 -0700

    png converts

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('graphics/fonts/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+#(text content, smoothing, color)
 text_surface = test_font.render('My game', False, 'Black')
 
-snail_surface = pygame.image.load('graphics/snail1.png')
+snail_surface = pygame.image.load('graphics/snail1.png').convert_alpha()
 snail_x_pos = 850
 
 while True:
